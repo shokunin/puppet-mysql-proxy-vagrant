9272866012915185f95154c0a5280af3715ac83c
Chris Mague <github@mague.com>
2016-06-16 15:55:22 -0700

    syntax

--- puppet/manifests/mysql.pp
+++ puppet/manifests/mysql.pp
@@ -10,7 +10,7 @@ class { 'mysqlproxy':
   install_version => 'mysql-proxy-0.8.5-linux-glibc2.3-x86-64bit',
   address         => '0.0.0.0:3307',
   #  luascript       => 'mc.lua',
-  options         => '--log-file=/tmp/proxy.log --log-level=info'
+  options         => '--log-file=/tmp/proxy.log --log-level=info',
   backends        => ['localhost:3306']
 }
 
